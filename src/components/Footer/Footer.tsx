aa2d0f19c6d69dba5a77855fd9ad9e4154efd576
schuntikov.viktor@yandex.ru <schuntikov.viktor@yandex.ru>
2025-12-08 04:06:43 +0700

    wip: footer in progress

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.scss';
+
+
+export const Footer = () => {
+  return (
+    <section className="footer">
+      <div className="footer__inner container">
+
+      </div>
+    </section>
+  )
+}
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,17 +2,19 @@ import { Header } from "../components/Header";
 import { Hero } from "../components/Hero";
 import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
+import { Footer } from "../components/Footer";
 
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <main>
-        <Hero />
-        <Skills />
-        <Projects />
-      </main>
+        <main>
+          <Hero />
+          <Skills />
+          <Projects />
+        </main>
+      <Footer />
     </>
   );
 };
