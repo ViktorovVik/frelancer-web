d3107565917a834c8c985a8a8eb0c9b84546e004
schuntikov.viktor@yandex.ru <schuntikov.viktor@yandex.ru>
2025-12-13 18:22:07 +0700

    feat: add Redux store and user slice

---
+++ src/store/user.slice.ts
@@ -0,0 +1,26 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export interface UserState {
+  jwt: string | null;
+}
+
+const initialState: UserState = {
+  jwt: null
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addJwt: (state) => {
+      state.jwt = 'asdasdad'
+    },
+    logout: (state) => {
+      state.jwt = null;
+    }
+  }
+});
+
+
+export default userSlice.reducer;
+export const userActions = userSlice.actions;
